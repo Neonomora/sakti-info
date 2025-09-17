503beb6e3f77ccdaa3a017618ecf263254d41e6a
unknown <abdillahas443@gmail.com>
2025-09-17 09:05:27 +0700

    Hide hash.js

--- hash.js
+++
@@ -1,8 +0,0 @@
-// hash.js
-import bcrypt from 'bcryptjs';
-
-const password = 'correcthorsebatterystaple';
-
-bcrypt.hash(password, 10).then((hash) => {
-  console.log("Hashed password:", hash);
-});
